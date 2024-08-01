a22622a1d6cce324661170e7ba520d5980fae1a1
abko27700 <abko27700@gmail.com>
2024-08-01 19:24:51 -0400

    added before install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,3 @@
-touch /home/ec2-user/projectDaria/subsBackend/logs/deployment.log
+touch /home/ec2-user/projectDaria/SubscriptionReminderBackend/logs/deployment.log
 
-echo "Code updated" >> /home/ec2-user/projectDaria/subsBackend/logs/deployment.log
+echo "Code updated" >> /home/ec2-user/projectDaria/SubscriptionReminderBackend/logs/deployment.log
